62a9a0b1b211de84a040868f02d69547a0d0b0f0
volcano1998 <54039319+volcano1998@users.noreply.github.com>
2021-09-21 16:29:45 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(name='Mars',      
+version='1.0',      
+description='Multiple Alignment-based Refinement of SVs',      
+author='Xin Zhou',      
+author_email='mazie.zhou@vanderbilt.edu',      
+install_requires=['Bio','pysam','pandas','minimap2','openpyxl','samtools','k8'],         
+license='MIT',      
+packages=['bin',],
+package_data={'bin' : ['paftools/*',
+                     'k8-0.2.4/*',
+                     'trf_tools/*',
+                     ]},  
+entry_points={'console_scripts':['MARS_step1=bin.MARS_step1:main',
+'MARS_step2=bin.MARS_step2:main',
+'MARS_step3=bin.MARS_step3:main'
+               ]},
+zip_safe=False)
